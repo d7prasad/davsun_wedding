cd226c68c3b81a255fc33f1e07b48b37305170bd
David Prasad <d7prasad@gmail.com>
2018-07-18 19:18:56 +0530

    added wedding text field

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,16 @@
 // Animate Smooth Scroll
-$('#view-work').on('click', function() {
-  const images = $('#images').position().top;
+// $('#view-work').on('click', function() {
+//   const images = $('#images').position().top;
 
-  $('html, body').animate(
-    {
-      scrollTop: images
-    },
-    900
-  );
-});
+//   $('html, body').animate(
+//     {
+//       scrollTop: images
+//     },
+//     900
+//   );
+// });
+
+$('#wedding-form').hide();
+$('#wedding-Details').on('click', function(){
+  $('#wedding-form').toggle();
+})
