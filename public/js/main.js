2e1cdeeb25b77a194b8389866b11d669997037fc
David <d7prasad@gmail.com>
2018-07-21 12:38:53 +0530

    Node with buttons

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,23 @@
-// Animate Smooth Scroll
-$('#view-work').on('click', function() {
-  const images = $('#images').position().top;
+// // Animate Smooth Scroll
+// $('#venue-Details').on('click', function() {
+//   const images = $('#header').position().top;
 
-  $('html, body').animate(
-    {
-      scrollTop: images
-    },
-    900
-  );
+//   $('html, body').animate(
+//     {
+//       scrollTop: images
+//     },
+//     900
+//   );
+// });
+$('#wedding-input').hide();
+$('#wedding-Details').on('click', function() {
+  $('#wedding-input').toggle();
+});
+
+$('#wedding-vote-cancel').on('click', function(){
+  $('#wedding-input').hide();
+})
+
+$("#wedding-vote-submit").on('click', function(){
+  alert($("input[name=wedding-vote]:checked").val())
 });
